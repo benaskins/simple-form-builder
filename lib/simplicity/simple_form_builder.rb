02eb5d8fdb0a12de4247c9b49df0b57ff789b345
Ben Askins <ben.askins@gmail.com>
2009-10-20 10:54:46 +1100

    Patches from Weather Engine.

--- lib/simplicity/simple_form_builder.rb
+++ lib/simplicity/simple_form_builder.rb
@@ -11,7 +11,7 @@ module Simplicity
       form_for(record_or_name_or_array, *(args << options.merge(:builder => SimpleFormBuilder)), &proc)
     end
     
-    def smart_fields_for(record_or_name_or_array, *args, &proc)
+    def simple_fields_for(record_or_name_or_array, *args, &proc)
       options = args.extract_options!
       fields_for(record_or_name_or_array, *(args << options.merge(:builder => SimpleFormBuilder)), &proc)
     end    
@@ -25,11 +25,11 @@ module Simplicity
     end
 
     def date_select(method, options={})
-      labelled_field(method, options, super)
+      list_item(label(method, options) + super, :class => "dates")
     end
 
     def time_select(method, options={})
-      labelled_field(method, options, super)
+      list_item(label(method, options) + super, :class => "dates")
     end
 
     def text_field(method, options={})
@@ -44,12 +44,12 @@ module Simplicity
       labelled_field(method, options, super)
     end
 
-    def check_box(field, options={}, checked_value="1", unchecked_value="0")
-      labelled_field(method, options, super)
+    def check_box(method, options={}, checked_value="1", unchecked_value="0")
+      list_item(super + label(method, options.merge(:no_colon => true)), :class => "checkbox")
     end
 
     def radio_button(method, tag_value, options={})
-      list_item(super + radio_label(method, tag_value, options))
+      super + radio_label(method, tag_value, options)
     end
 
     def select(method, choices, options={}, html_options={})
@@ -57,7 +57,7 @@ module Simplicity
     end
 
     def collection_select(method, collection, value_method=:id, text_method=:name, options={}, html_options={})
-      field_group(label(method, options) + super)
+      list_item(label(method, options) + super)
     end
 
     def country_select(method, priority_countries=nil, options={}, html_options={})
@@ -72,9 +72,16 @@ module Simplicity
       labelled_field(method, options, super)
     end
 
-    def fieldset(legend=nil, &block)
-      @template.concat @template.tag(:fieldset, {:class => "set"}, true)
-      @template.concat @template.content_tag(:h3, legend) unless legend.nil?
+    def title(text)
+      @template.content_tag(:h3, text)
+    end
+    
+    def hint(text)
+      @template.content_tag(:p, text)
+    end
+
+    def fieldset(options={}, &block)
+      @template.concat @template.tag(:fieldset, options.merge(:class => "set"), true)
       @template.concat @template.tag(:ol, nil, true)
 
       yield
@@ -93,7 +100,7 @@ module Simplicity
 
     def button(options={})
       prefix = @object.new_record? ? "Create" : "Save"
-      label = "#{prefix} #{@object_name.humanize}"
+      label = "#{prefix} #{options[:label] || @object_name.humanize}"
       list_item(
         @template.content_tag(:button, 
           @template.content_tag(:img, nil, :src => options[:img], :alt => label) + label, 
@@ -130,6 +137,14 @@ module Simplicity
         markup + label(method, options)
       else
         list_item(label(method, options) + markup)
+      end
+    end
+
+    def reversed_labelled_field(method, options, markup)
+      if @already_grouping
+        markup + label(method, options)
+      else
+        list_item(markup + label(method, options))
       end
     end
 
